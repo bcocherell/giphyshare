88e8c3a1e404ae036273b44266a33661f52a62eb
Peter Burns <peterburnshicks@gmail.com>
2018-02-15 09:58:34 -0500

    added commentSection & post partials, added sample post

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,16 +2,24 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.render("index");
 });
 
-router.get("/posts", function(req, res) {
+router.get("/posts", function (req, res) {
     res.render("posts");
 });
 
-router.get("/post", function(req, res) {
-    res.render("post");
+router.get("/post", function (req, res) {
+    res.render("post", {
+        post:"assets/img/Bodie.jpg",
+        comments: [{
+            text: 'Hello this is the first comment'
+        }, {
+         text: 'Comment the second'
+    }
+        ]
+    });
 });
 
 
