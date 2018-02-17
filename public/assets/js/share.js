b2a126453daa9e9dcfbe8b88f1c248158b04f77c
Peter Burns <peterburnshicks@gmail.com>
2018-02-17 12:16:51 -0500

    added share button functionality changed search post to use giphy api format, made post follow db format

--- public/assets/js/share.js
+++ public/assets/js/share.js
@@ -1,12 +1,20 @@
-$('.shareBtn').on('click', function(){
-    $('.modalPicContainer').html(`<img class="modalPic" src=${$('.postPic').attr('src')}>`);
+$('.shareBtn').on('click', function () {
+    $('#modalPicContainer').empty();
+    var src= $(this).parent().parent().children('.postPic');
+    $('#modalPicContainer').append(src);
 });
-$('.postBtn').on('click', function(event){
-    $.post('/api/posts', {
-        url:asd,
-        urlStill:asdasd,
-        urlOriginal:asdasd,
-        urlOriginalStill:asdasd,
-        comment:comment
+
+$('.postBtn').on('click', function (event) {
+    var img = $(this).parent().parent().children('.modal-body').children('#modalPicContainer').children('.postPic');
+    $.ajax('/api/posts', {
+        type: 'POST',
+        data: {
+            url: img.attr('src'),
+            urlStill: img.attr('data-fixed'),
+            urlOriginal: img.attr('data-original'),
+            urlOriginalStill: img.attr('data-fixed-small'),
+            comment: $('#modalComment').val(),
+            userId: 1
+        }
     });
 });
