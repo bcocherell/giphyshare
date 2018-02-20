d889b5ec8a392f1ab8091f1a2f4160632d0b27ad
Pete Burns <peterburnshicks@gmail.com>
2018-02-19 22:16:00 -0500

    fixed formatting on post page and added alert to comment follow button

--- public/assets/js/followBtn.js
+++ public/assets/js/followBtn.js
@@ -3,11 +3,15 @@ $('#followBtn').on('click', function (event) {
     $.ajax('/api/followers', {
         type: 'POST',
         data: {
-            followerId:$(this).attr('data-id'),
-            userId:$(this).attr('data-user'),
-            status:true
+            followerId: $(this).attr('data-id'),
+            userId: $(this).attr('data-user'),
+            status: true
         }
-    }).then(function(){
-        location.reload();
+    }).then(function () {
+        $('nav').after('<div class="alert alert-primary" role="alert">Followed!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+        if (alertTimeout) clearTimeout(alertTimeout);
+        alertTimeout = setTimeout(function () {
+            $('.alert').remove();
+        }, 5000);
     });
 });
