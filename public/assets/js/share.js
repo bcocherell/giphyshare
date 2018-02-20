8361b7ae071fa3016fcb830f9facaf69e469ebd4
Pete Burns <peterburnshicks@gmail.com>
2018-02-20 12:18:40 -0500

    added signUpModal, semantic change to navbar signup, sharemodal buttons and comment form changed to be real forms with submit buttons

--- public/assets/js/share.js
+++ public/assets/js/share.js
@@ -10,7 +10,7 @@ $('.shareBtn').on('click', function (event) {
 
 $('.postBtn').on('click', function (event) {
     event.preventDefault();
-    var img = $(this).parent().parent().children('.modal-body').children('#modalPicContainer').children('.postPic');
+    var img = $(this).parent().parent().parent().children('#modalPicContainer').children('.postPic');
     $.ajax('/api/posts', {
         type: 'POST',
         data: {
