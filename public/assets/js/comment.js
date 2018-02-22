0534f6dec13f2159dd589a72001ef5c5244126ff
Pete Burns <peterburnshicks@gmail.com>
2018-02-22 09:36:45 -0500

    standardized quote format and updated user page to not highlight navbar

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -1,11 +1,11 @@
-$('#submitComment').on('click', function (event) {
+$("#submitComment").on("click", function (event) {
     event.preventDefault();
-    $.ajax('/api/subposts', {
-        type: 'POST',
+    $.ajax("/api/subposts", {
+        type: "POST",
         data: {
-            userId:$(this).attr('data-user'),
-            postId:$(this).attr('data-id'),
-            comment: $('#subPostComment').val(),
+            userId:$(this).attr("data-user"),
+            postId:$(this).attr("data-id"),
+            comment: $("#subPostComment").val(),
         }
     }).then(function(){
         location.reload();
