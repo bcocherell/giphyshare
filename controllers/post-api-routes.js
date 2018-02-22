97032cacf860a02479b719f4076212023f4c0627
Brian Cocherell <brian.d.cocherell@sherwin.com>
2018-02-21 22:13:51 -0500

    cleaning up routes

--- controllers/post-api-routes.js
+++ controllers/post-api-routes.js
@@ -2,64 +2,9 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-  // get all public posts, includes user info
-  app.get("/api/posts", function(req, res) {
-    db.post.findAll({
-      include: [db.user],
-      order: [
-        ['createdAt', 'DESC']
-      ]
-    }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
-  // get all user post for particular userId
-  app.get("/api/posts/:id", function(req, res) {
-    db.post.findAll({
-      where: { userId: req.params.id },
-      order: [
-        ['createdAt', 'DESC']
-      ]
-    }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
-  // get post by postId, includes subpost data
-  app.get("/api/post/:id", function(req, res) {
-    db.post.findAll({
-      include: [db.subpost],
-      where: { id: req.params.id }
-    }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
   // insert post
   app.post("/api/posts", function(req, res) {
     db.post.create(req.body).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
-  // posts of all followers for particular userId
-  app.get("/api/followerposts/:id", function(req, res) {
-    db.post.findAll({
-      include: [{
-        model: db.user,
-        required: true,
-        include: [{
-          model: db.follower,
-          as: 'Followers',
-          where: {userId: req.params.id},
-          required: true
-        }]
-      }],
-      order: [
-        ['createdAt', 'DESC']
-      ]
-    }).then(function(dbPost) {
       res.json(dbPost);
     });
   });
--- controllers/user-api-routes.js
+++ controllers/user-api-routes.js
@@ -2,22 +2,6 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-  // get all users
-  app.get("/api/users", function(req, res) {
-    db.user.findAll({}).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
-
-  // get user info by username
-  app.get("/api/users/:id", function(req, res) {
-    db.user.findOne({
-      where: {id: req.params.id}
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
-
   // create new user
   app.post("/api/users", function(req, res) {
     db.user.create(req.body).then(function(dbUser) {
