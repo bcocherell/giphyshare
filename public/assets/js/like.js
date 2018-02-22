b1c6687cf5468d8c3648f580ac0743fc3f25739d
Pete Burns <peterburnshicks@gmail.com>
2018-02-22 09:16:27 -0500

    added like counter and updater

--- public/assets/js/like.js
+++ public/assets/js/like.js
@@ -1,10 +1,30 @@
 $('.likeBtn').on('click', function (event) {
     event.preventDefault();
+    var self=this;
     $.ajax('/api/likes', {
         type: 'POST',
         data: {
             postId:$(this).attr('data-postid'),
             userId:$(this).attr('data-user')
+        }
+    }).then(function(){
+        $.ajax('/api/likes/' + $(self).attr('data-postid'), {
+            type: 'GET'
+        }).then(function(res){
+            console.log(res);
+            if(res.length){
+                $(self).prepend('<b>' + res.length + '</b>');
+            }
+        });
+    });
+});
+
+$('.likeBtn').each(function(index, button){
+    $.ajax('/api/likes/' + $(button).attr('data-postid'), {
+        type: 'GET'
+    }).then(function(res){
+        if(res.length){
+            $(button).prepend('<b>' + res.length + '</b>');
         }
     });
 });
