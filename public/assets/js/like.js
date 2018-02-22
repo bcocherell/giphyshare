2492fee853e27a96d0b6f093db531a384a4f3c32
Pete Burns <peterburnshicks@gmail.com>
2018-02-22 09:45:00 -0500

    fix for like btn to update correctly

--- public/assets/js/like.js
+++ public/assets/js/like.js
@@ -1,29 +1,30 @@
-$('.likeBtn').on('click', function (event) {
+$(".likeBtn").on("click", function (event) {
     event.preventDefault();
     var self=this;
-    $.ajax('/api/likes', {
-        type: 'POST',
+    $.ajax("/api/likes", {
+        type: "POST",
         data: {
-            postId:$(this).attr('data-postid'),
-            userId:$(this).attr('data-user')
+            postId:$(this).attr("data-postid"),
+            userId:$(this).attr("data-user")
         }
     }).then(function(){
-        $.ajax('/api/likes/' + $(self).attr('data-postid'), {
-            type: 'GET'
+        $.ajax("/api/likes/" + $(self).attr("data-postid"), {
+            type: "GET"
         }).then(function(res){
             if(res.length){
-                $(self).prepend('<b>' + res.length + '</b>');
+                $(self).children('b').remove();
+                $(self).prepend("<b>" + res.length + "</b>");
             }
         });
     });
 });
 
-$('.likeBtn').each(function(index, button){
-    $.ajax('/api/likes/' + $(button).attr('data-postid'), {
-        type: 'GET'
+$(".likeBtn").each(function(index, button){
+    $.ajax("/api/likes/" + $(button).attr("data-postid"), {
+        type: "GET"
     }).then(function(res){
         if(res.length){
-            $(button).prepend('<b>' + res.length + '</b>');
+            $(button).prepend("<b>" + res.length + "</b>");
         }
     });
 });
